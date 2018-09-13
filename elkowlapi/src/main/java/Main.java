1ad788b2ce928f1155db4e5cd981099b209fa275
katuskaa <kfabianova11@gmail.com>
2018-09-13 15:40:41 +0200

    updated logger

--- elkowlapi/src/main/java/Main.java
+++ elkowlapi/src/main/java/Main.java
@@ -1,10 +1,13 @@
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
 public class Main {
 
     public static void main(String[] args) throws OWLOntologyCreationException {
 
+        Logger.getRootLogger().setLevel(Level.OFF);
         BasicConfigurator.configure();
 
         Ontology ontology = new Ontology();
